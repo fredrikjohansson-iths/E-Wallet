86cdcb67334d00d12dc3ebd7df67ea601fb4b546
Fredrik Johansson <fredrik.johansson@ith.se>
2021-01-19 15:02:29 +0100

    Initial commit

---
+++ src/views/AddCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <Heading />
+    <BlankCard />
+    <Form />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Heading from "@/components/AddCard/Heading.vue";
+import BlankCard from "@/components/AddCard/BlankCard.vue";
+import Form from "@/components/AddCard/Form.vue";
+
+export default {
+  name: "AddCard",
+  components: {
+    Heading,
+    BlankCard,
+    Form
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <Heading />
+    <ActiveCard />
+    <CardStack />
+    <AddNewCard />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Heading from "@/components/Home/Heading.vue";
+import ActiveCard from "@/components/Home/ActiveCard.vue";
+import CardStack from "@/components/Home/CardStack.vue";
+import AddNewCard from "@/components/Home/AddNewCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Heading,
+    ActiveCard,
+    CardStack,
+    AddNewCard
+  }
+};
+</script>
